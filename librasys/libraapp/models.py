c39738562be02808886e98c339debb6400bf9649
arunp007 <arunp4624@gmail.com>
2023-08-22 15:48:49 +0530

    new commit

--- librasys/libraapp/models.py
+++ librasys/libraapp/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    firstname = models.TextField(max_length=50)
+    lastname = models.TextField(max_length=50)
+    day = models.TextField(max_length=50)
+    month = models.TextField(max_length=50)
+    year = models.TextField(max_length=50)
+    gender = models.TextField(max_length=50)
+    address = models.TextField(max_length=50)
+    location = models.TextField(max_length=50)
+    username = models.TextField(max_length=50)
+    email = models.TextField(max_length=50)
+    phone = models.TextField(max_length=50)
+    password = models.TextField(max_length=50)
+    cpassword = models.TextField(max_length=50)
+
+class Meta:
+    db_table = 'user_registration'
+
+
+class Librarian(models.Model):
+    firstname = models.TextField(max_length=50)
+    lastname = models.TextField(max_length=50)
+    library_name = models.TextField(max_length=50)
+    library_location = models.TextField(max_length=50)
+    email = models.TextField(max_length=50)
+    phone = models.TextField(max_length=50)
+    password = models.TextField(max_length=50)
+    cpassword = models.TextField(max_length=50)
+
+class Meta:
+    db_table = 'librarian_registration'
+
+
+    
+class Admin(models.Model):
+    firstname = models.TextField(max_length=50)
+    lastname = models.TextField(max_length=50)
+    email = models.TextField(max_length=50)
+    phone = models.TextField(max_length=50)
+    password = models.TextField(max_length=50)
+    cpassword = models.TextField(max_length=50)
+
+class Meta:
+    db_table = 'admin_registration'
+
+
+
+
--- librasys/libraapp/views.py
+++ librasys/libraapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from.models import *
 
 def user_home(request):
     return render(request, 'user_home.html')
@@ -8,6 +9,22 @@ def user_login(request):
     return render(request, 'user_login.html')
 
 def user_signup(request):
+    if request.method == 'POST':
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        day = request.POST['day']
+        month = request.POST['month']
+        year = request.POST['year']
+        gender = request.POST['gender']
+        address = request.POST['address']
+        location = request.POST['location']
+        username = request.POST['username']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+        user_details = User(firstname = fname, lastname = lname, day = day, month = month, year = year, gender = gender, address = address, location = location, username = username, email = email, phone = phone, password = password, cpassword = cpassword)
+        user_details.save()
     return render(request, 'user_signup.html')
 
 def librarian_home(request):
@@ -17,6 +34,17 @@ def librarian_login(request):
     return render(request, 'librarian_login.html')
 
 def librarian_signup(request):
+    if request.method == 'POST':
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        library_name = request.POST['library_name']
+        library_location = request.POST['library_location']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+        librarian_details = Librarian(firstname = fname, lastname = lname, library_name = library_name, library_location = library_location, email = email, phone = phone, password = password, cpassword = cpassword)
+        librarian_details.save()
     return render(request, 'librarian_signup.html')
 
 def admin_home(request):
@@ -26,4 +54,13 @@ def admin_login(request):
     return render(request, 'admin_login.html')
 
 def admin_signup(request):
+    if request.method == 'POST':
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+        admin_details = Admin(firstname = fname, lastname = lname, email = email, phone = phone, password = password, cpassword = cpassword)
+        admin_details.save()
     return render(request, 'admin_signup.html')
